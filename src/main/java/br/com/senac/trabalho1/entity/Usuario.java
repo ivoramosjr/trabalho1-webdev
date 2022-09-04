2729b0e5ff5d8fd82e46777fb165a6b10bc49739
Ivo Ramos Junior <ivo.ramos.94@gmail.com>
2022-09-04 17:20:39 -0300

    Criado a entidade Usuario

---
+++ src/main/java/br/com/senac/trabalho1/entity/Usuario.java
@@ -0,0 +1,46 @@
+package br.com.senac.trabalho1.entity;
+
+public class Usuario {
+
+    private long id;
+
+    private String nome;
+
+    private String senha;
+
+    public Usuario() {
+    }
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getSenha() {
+        return senha;
+    }
+
+    public void setSenha(String senha) {
+        this.senha = senha;
+    }
+
+    @Override
+    public String toString() {
+        return "Usuario{" +
+                "id=" + id +
+                ", nome='" + nome + '\'' +
+                ", senha='" + senha + '\'' +
+                '}';
+    }
+}
