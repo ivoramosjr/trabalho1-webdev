e173268494a123fdd119c1d6ae1884f8cffa52bd
Ivo Ramos Junior <ivo.ramos.94@gmail.com>
2022-09-04 18:16:25 -0300

    Refatorado repository

--- src/main/java/br/com/senac/trabalho1/repository/LoginRepository.java
+++ src/main/java/br/com/senac/trabalho1/repository/LoginRepository.java
@@ -7,31 +7,15 @@ import java.util.List;
 
 public class LoginRepository {
 
-    List<Usuario> usuarios = new ArrayList<>();
+    public LoginRepository(){}
 
-    public LoginRepository(){
-        usuarios = carregarUsuarios();
-    }
-
-    private List<Usuario> carregarUsuarios() {
+    public List<Usuario> carregarUsuarios() {
         List<Usuario> usuariosEntity = new ArrayList<>();
         usuariosEntity.add(new Usuario(1, "admin", "admin123"));
         usuariosEntity.add(new Usuario(2, "ivo", "ivo123"));
         usuariosEntity.add(new Usuario(3, "thiago", "thiago123"));
         usuariosEntity.add(new Usuario(4, "leozin", "leozin123"));
         return usuariosEntity;
-    }
-
-    public boolean validaLogin(Usuario usuario){
-        if(usuario.getLogin() == null || usuario.getSenha() == null)
-            return false;
-
-        if(usuario.getLogin().equals("") || usuario.getSenha().equals(""))
-            return false;
-
-        return usuarios.stream()
-                .anyMatch(u -> u.getLogin().equals(usuario.getLogin())
-                        && u.getSenha().equals(usuario.getSenha()));
     }
 
 }
