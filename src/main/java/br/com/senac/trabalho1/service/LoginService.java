ffcbda5ec5d72a94dae5fdbcbb55f7ca3fb675f4
Ivo Ramos Junior <ivo.ramos.94@gmail.com>
2022-09-04 18:18:07 -0300

    Refatorado service

--- src/main/java/br/com/senac/trabalho1/service/LoginService.java
+++ src/main/java/br/com/senac/trabalho1/service/LoginService.java
@@ -3,6 +3,8 @@ package br.com.senac.trabalho1.service;
 import br.com.senac.trabalho1.entity.Usuario;
 import br.com.senac.trabalho1.repository.LoginRepository;
 
+import java.util.List;
+
 public class LoginService {
 
     private LoginRepository loginRepository;
@@ -12,7 +14,22 @@ public class LoginService {
     }
 
     public boolean validarLogin(Usuario usuario){
-        return loginRepository.validaLogin(usuario);
+        return validaLogin(usuario);
+    }
+
+
+    private boolean validaLogin(Usuario usuario){
+        List<Usuario> usuarios = loginRepository.carregarUsuarios();
+
+        if(usuario.getLogin() == null || usuario.getSenha() == null)
+            return false;
+
+        if(usuario.getLogin().equals("") || usuario.getSenha().equals(""))
+            return false;
+
+        return usuarios.stream()
+                .anyMatch(u -> u.getLogin().equals(usuario.getLogin())
+                        && u.getSenha().equals(usuario.getSenha()));
     }
 
 }
